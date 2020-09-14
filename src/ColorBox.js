46f3ec9646216edad1bae8c122882649ce4fac13
Tneebs <tedneben@gmail.com>
2020-09-13 23:25:32 -0500

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -9,12 +9,24 @@ export default class ColorBox extends Component {
   }
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
-      </div>
-    )
-  }
-
+    if (this.props.opacity >= 0.2) {
+      return (
+        <div className='color-box' style={{opacity: this.props.opacity}}>
+          <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+      )} else {
+        return null
+      }
+    }
 }
 
+
+// Trying to get this passing by using the Ternary Expression but not doing it right. Used if/else instead. 
+//
+//   render() {
+//     const newOpacity = this.props.opacity - 0.1;
+//     return this.props.opacity >= 0.2 ? null : ( <div className="color-box" style={{opacity: this.props.opacity}}>
+//         <ColorBox opacity={newOpacity} />
+//       </div>)
+//   }
+// }
